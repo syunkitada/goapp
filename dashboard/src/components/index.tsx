0bc10b4b9d1709e5fb637209fd9851fd92ebb1d4
syunkitada <syun.kitada@gmail.com>
2020-04-05 23:00:59 +0900

    update

--- dashboard/src/components/index.tsx
+++ dashboard/src/components/index.tsx
@@ -11,57 +11,67 @@ import IndexView from "./views/IndexView";
 import logger from "../lib/logger";
 
 function renderIndex(routes, data, index) {
-  if (!index) {
-    return <div>Not Found</div>;
-  }
-  logger.info("Index", "renderIndex:", index.Kind, routes);
-  switch (index.Kind) {
-    case "Msg":
-      return <div>{index.Name}</div>;
-    case "RoutePanels":
-      return (
-        <RoutePanels
-          render={renderIndex}
-          routes={routes}
-          data={data}
-          index={index}
-        />
-      );
-    case "RouteTabs":
-      return (
-        <Tabs render={renderIndex} routes={routes} data={data} index={index} />
-      );
-    case "RoutePanes":
-      return (
-        <Panes render={renderIndex} routes={routes} data={data} index={index} />
-      );
-    case "Table":
-      return (
-        <IndexTable
-          render={renderIndex}
-          routes={routes}
-          index={index}
-          data={data}
-        />
-      );
-    case "View":
-      return (
-        <IndexView
-          render={renderIndex}
-          routes={routes}
-          index={index}
-          data={data}
-        />
-      );
-    case "SearchForm":
-      return <SearchForm routes={routes} index={index} data={data} />;
-    case "Form":
-      return <IndexForm routes={routes} index={index} data={data} />;
-    default:
-      return <div>Unsupported Kind: {index.Kind}</div>;
-  }
+    if (!index) {
+        return <div>Not Found</div>;
+    }
+    logger.info("Index", "renderIndex:", index.Kind, routes);
+    switch (index.Kind) {
+        case "Msg":
+            return <div>{index.Name}</div>;
+        case "RoutePanels":
+            return (
+                <RoutePanels
+                    render={renderIndex}
+                    routes={routes}
+                    data={data}
+                    index={index}
+                />
+            );
+        case "RouteTabs":
+            return (
+                <Tabs
+                    render={renderIndex}
+                    routes={routes}
+                    data={data}
+                    index={index}
+                />
+            );
+        case "RoutePanes":
+            return (
+                <Panes
+                    render={renderIndex}
+                    routes={routes}
+                    data={data}
+                    index={index}
+                />
+            );
+        case "Table":
+            return (
+                <IndexTable
+                    render={renderIndex}
+                    routes={routes}
+                    index={index}
+                    data={data}
+                />
+            );
+        case "View":
+            return (
+                <IndexView
+                    render={renderIndex}
+                    routes={routes}
+                    index={index}
+                    data={data}
+                />
+            );
+        case "SearchForm":
+            return <SearchForm routes={routes} index={index} data={data} />;
+        case "Form":
+            return <IndexForm routes={routes} index={index} data={data} />;
+        default:
+            return <div>Unsupported Kind: {index.Kind}</div>;
+    }
 }
 
 export default {
-  renderIndex
+    renderIndex
 };
