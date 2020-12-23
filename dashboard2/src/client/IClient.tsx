dddd8d4ea10eba5e89d67bcfdcbfeb8f4d3c066a
syunkitada <syun.kitada@gmail.com>
2020-12-23 20:42:05 +0900

    refactoring

--- dashboard2/src/client/IClient.tsx
+++
@@ -1,6 +0,0 @@
-export interface IClient {
-    loginWithToken(input: any): void;
-    login(input: any): void;
-    logout(input: any): void;
-    get_service_index(input: any): void;
-}
--- dashboard2/src/client/index.tsx
+++
@@ -1,5 +0,0 @@
-import goapp from "./goapp";
-import { IClient } from "./IClient";
-
-const client: IClient = new goapp.Client();
-export default client;
