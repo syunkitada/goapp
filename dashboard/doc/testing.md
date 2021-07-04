2c8ce7e671ac60cdf836ec2460f1ddc27824c838
syunkitada <syun.kitada@gmail.com>
2021-07-04 12:11:36 +0900

    rm dashboard

--- dashboard/doc/testing.md
+++
@@ -1,5 +0,0 @@
-# Testing
-
-* https://create-react-app.dev/docs/running-tests
-* https://reactjs.org/docs/testing-recipes.html
-
--- dashboard/src/lib/sort_utils/index.tsx
+++
@@ -1,7 +0,0 @@
-import getSorting from './getSorting';
-import stableSort from './stableSort';
-
-export default {
-    getSorting,
-    stableSort,
-}
